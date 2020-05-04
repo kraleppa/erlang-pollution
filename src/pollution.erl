e6e85732d20345e375c7f7b351932f991badd637
kraleppa <misiek.nalepa@wp.pl>
2020-05-04 19:39:27 +0200

    basics done

---
+++ src/pollution.erl
@@ -0,0 +1,133 @@
+%%%-------------------------------------------------------------------
+%%% @author krzysztof
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 29. mar 2020 15:46
+%%%-------------------------------------------------------------------
+-module(pollution).
+-author("krzysztof").
+
+%% API
+-export([createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4, getStationMean/3, getDailyMean/3, getDailyOverLimit/4, getYearlyMean/3, getStationByCords/2]).
+
+-record(monitor, {stations, data}).
+
+%% inicjalizuje monitor
+createMonitor() -> #monitor{stations = #{}, data = #{}}.
+
+%% sprawdza czy dana stacja istnieje
+stationExists(Name, Cords, Monitor) ->
+  Map1 = maps:filter(fun(Station, _) -> Station == Name end, Monitor#monitor.stations),
+  Map2 = maps:filter(fun(_, KeyCords) -> KeyCords == Cords end, Monitor#monitor.stations),
+  (maps:size(Map1) /= 0) or (map_size(Map2) /= 0).
+
+stationExists(Name, Monitor) ->
+  Map1 = maps:filter(fun(Station, _) -> Station == Name end, Monitor#monitor.stations),
+  maps:size(Map1) /= 0.
+
+%% dodaje stacje
+addStation(Name, {X, Y}, Monitor) when is_list(Name) and is_number(X) and is_number(Y) and is_record(Monitor, monitor)->
+  case stationExists(Name, {X, Y}, Monitor) of
+    false -> #monitor{stations = maps:put(Name, {X, Y}, Monitor#monitor.stations), data = Monitor#monitor.data};
+    _ -> {error, "Station already exists"}
+  end.
+
+%% sprawdza czy dany pomiar zostal juz zapisany
+valueExists(Tuple, Monitor) ->
+  Map = maps:filter(fun(Key, _) -> Key == Tuple end, Monitor#monitor.data),
+  maps:size(Map) /= 0.
+
+
+getStationByCords(Tuple, Monitor) when is_tuple(Tuple) and is_record(Monitor, monitor) ->
+  Map = maps:filter(fun(_, Cords) -> Cords == Tuple end, Monitor#monitor.stations),
+  List = maps:keys(Map),
+  case length(List) of
+    1 -> [Head | _] = List, Head;
+    _ -> "Station not Found"
+  end.
+
+
+%% dodaje pomiar
+addValue(Tuple, Date, Type, Value, Monitor) when is_tuple(Tuple) and is_list(Type) and is_number(Value) and is_record(Monitor, monitor)->
+  Name = getStationByCords(Tuple, Monitor),
+  addValue(Name, Date, Type, Value, Monitor);
+
+addValue(Name, Date, Type, Value, Monitor) when is_list(Name) and is_list(Type) and is_number(Value) and is_record(Monitor, monitor)->
+  case calendar:valid_date(Date) and not valueExists({Name, Date, Type}, Monitor) of
+    true -> #monitor{stations = Monitor#monitor.stations, data = maps:put({Name, Date, Type}, Value, Monitor#monitor.data)};
+    _ -> {error, "Value already exists"}
+  end.
+
+
+%% ususwa pomiar
+removeValue(Tuple, Date, Type, Monitor) when is_tuple(Tuple) and is_list(Type) and is_record(Monitor, monitor) ->
+  Name = getStationByCords(Tuple, Monitor),
+  removeValue(Name, Date, Type, Monitor);
+
+removeValue(Name, Date, Type, Monitor) when is_list(Name) and is_list(Type) and is_record(Monitor, monitor) ->
+  case valueExists({Name, Date, Type}, Monitor) of
+    true -> #monitor{stations = (Monitor#monitor.stations), data = maps:remove({Name, Date, Type}, Monitor#monitor.data)};
+    _ -> {error, "Value does not exist"}
+  end.
+
+
+
+%% pobiera wartosc pomiaru
+getOneValue(Tuple, Date, Type, Monitor) when is_tuple(Tuple) and is_list(Type) and is_record(Monitor, monitor) ->
+  Name = getStationByCords(Tuple, Monitor),
+  getOneValue(Name, Date, Type, Monitor);
+
+getOneValue(Name, Date, Type, Monitor) when is_list(Name) and is_list(Type) and is_record(Monitor, monitor) ->
+  case stationExists(Name, Monitor) of
+    false -> {error, "Station does not exist"};
+    _ ->
+      case valueExists({Name, Date, Type}, Monitor) of
+        true -> maps:get({Name, Date, Type}, Monitor#monitor.data);
+        _ -> {error, "Value does not exist"}
+      end
+  end.
+
+%% pobiera srednia wartosc danego pomiaru na stacji
+getStationMean(Tuple, Type, Monitor) when is_tuple(Tuple) and is_list(Type) ->
+  Name = getStationByCords(Tuple, Monitor),
+  getStationMean(Name, Type, Monitor);
+
+getStationMean(Name, Type, Monitor) when is_list(Name) and is_list(Type) ->
+  case stationExists(Name, Monitor) of
+    false -> {error, "Station does not exist"};
+    _ ->
+      F = fun({KeyName, _, KeyType}, _) -> (Name == KeyName) and (Type == KeyType) end,
+      Map = maps:filter(F, (Monitor#monitor.data)),
+      Sum = maps:fold(fun(_, Value, Acc) -> Acc + Value end, 0, Map),
+      Sum / maps:size(Map)
+  end.
+
+
+%% pobiera srednia danego pomiaru danego dnia
+getDailyMean(Type, Date, Monitor) when is_list(Type) ->
+  case calendar:valid_date(Date) of
+    true ->
+      Map = maps:filter(fun({_, KeyDate, KeyType}, _) -> (Date == KeyDate) and (Type == KeyType) end, Monitor#monitor.data),
+      Sum = maps:fold(fun(_, Value, Acc) -> Acc + Value end, 0, Map),
+      Sum / maps:size(Map);
+    false -> {error, "Incorrect Date"}
+  end.
+
+%% zwraca ilosc pomiarow ktore danego dnia wyszly poza norme
+getDailyOverLimit(Type, Date, Limit, Monitor) when is_list(Type) and is_number(Limit) ->
+  case calendar:valid_date(Date) of
+    true ->
+      F = fun({_, KeyDate, KeyType}, Value) -> (Value > Limit) and (KeyType == Type) and (Date == KeyDate) end,
+      Map = maps:filter(F, (Monitor#monitor.data)),
+      maps:size(Map);
+    false -> {error, "Incorrect Date"}
+  end.
+
+%% zwraca roczna srednia
+getYearlyMean(Type, Year, Monitor) when is_list(Type) and is_number(Year) ->
+  F = fun({_, {KeyYear, _, _}, KeyType}, _) -> (Year == KeyYear) and (Type == KeyType) end,
+  Map = maps:filter(F, (Monitor#monitor.data)),
+  Sum = maps:fold(fun(_, Value, Acc) -> Acc + Value end, 0, Map),
+  Sum / maps:size(Map).
---
+++ src/pollution_gen_server.erl
@@ -0,0 +1,51 @@
+%%%-------------------------------------------------------------------
+%%% @author krzysztof
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%% @end
+%%%-------------------------------------------------------------------
+-module(pollution_gen_server).
+
+-behaviour(gen_server).
+
+-export([start_link/0, addStation/2, addValue/4, removeValue/3, showMonitor/0, getOneValue/3, getStationMean/2, getDailyMean/2, getDailyOverLimit/3, getYearlyMean/2, stop/0, crash/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+
+-define(SERVER, ?MODULE).
+
+start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, pollution:createMonitor(), []).
+init(Monitor) -> {ok, Monitor}.
+
+showMonitor() -> gen_server:call(?MODULE, showMonitor).
+
+crash() -> gen_server:cast(?MODULE, gen_server).
+addStation(Name, Cords) -> gen_server:cast(?MODULE, {addStation, [Name, Cords]}).
+addValue(Station, Date, Type, Value) -> gen_server:cast(?MODULE, {addValue, [Station, Date, Type, Value]}).
+removeValue(Station, Date, Type) ->  gen_server:cast(?MODULE, {removeValue, [Station, Date, Type]}).
+
+handle_cast({addStation, [Name, Cords]}, Monitor) -> handle_cast_result(pollution:addStation(Name, Cords, Monitor), Monitor);
+handle_cast({addValue, [Station, Date, Type, Value]}, Monitor) -> handle_cast_result(pollution:addValue(Station, Date, Type, Value, Monitor), Monitor);
+handle_cast({removeValue, [Station, Date, Type]}, Monitor) -> handle_cast_result(pollution:removeValue(Station, Date, Type, Monitor), Monitor);
+handle_cast(crash, Monitor) -> 1 / 0, {noreply, Monitor}.
+
+getOneValue(Station, Date, Type) -> gen_server:call(?MODULE, {getOneValue, [Station, Date, Type]}).
+getStationMean(Station, Type) -> gen_server:call(?MODULE, {getStationMean, [Station, Type]}).
+getDailyMean(Type, Date) -> gen_server:call(?MODULE, {getDailyMean, [Type, Date]}).
+getDailyOverLimit(Type, Date, Limit) -> gen_server:call(?MODULE, {getDailyOverLimit, [Type, Date, Limit]}).
+getYearlyMean(Type, Year) -> gen_server:call(?MODULE, {getYearlyMean, [Type, Year]}).
+stop() -> gen_server:call(?MODULE, terminate).
+
+handle_call({getOneValue, [Station, Date, Type]}, _From, Monitor) -> {reply, pollution:getOneValue(Station, Date, Type, Monitor), Monitor};
+handle_call({getStationMean, [Station, Type]}, _From, Monitor) -> {reply, pollution:getStationMean(Station, Type, Monitor), Monitor};
+handle_call({getDailyMean, [Type, Date]}, _From, Monitor) -> {reply, pollution:getDailyMean(Type, Date, Monitor), Monitor};
+handle_call({getDailyOverLimit, [Type, Date, Limit]}, _From, Monitor) -> {reply, pollution:getDailyOverLimit(Type, Date, Limit, Monitor), Monitor};
+handle_call({getYearlyMean, [Type, Year]}, _From, Monitor) -> {reply, pollution:getYearlyMean(Type, Year, Monitor), Monitor};
+handle_call(showMonitor, _From, Monitor) -> {reply, Monitor, Monitor};
+handle_call(terminate, _From, Monitor) -> {stop, normal, ok, Monitor}.
+
+handle_cast_result({error, Text}, Monitor) -> erlang:display({error, Text}), {noreply, Monitor};
+handle_cast_result(NewMonitor, _) -> {noreply, NewMonitor}.
+
+handle_info(_, Monitor) -> {noreply, Monitor}.
+
+terminate(Reason, Monitor) -> erlang:display(Monitor), Reason.
